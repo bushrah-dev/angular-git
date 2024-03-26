1e4b9ac46e23c12a8ffdd87558b0b3ba7a3b77df
bushrah <you@example.comBushrah>
2024-03-26 09:00:43 +0100

    initial

---
+++ docs/server/chunk-U3KU5BSO.mjs
@@ -0,0 +1,35 @@
+import './polyfills.server.mjs';
+import{a as l,b as e,c as t,d as a,e as n,f as p,m,o as s,p as d,s as c,w as f,z as g}from"./chunk-4VBI6W4Z.mjs";var x=(()=>{let i=class i{constructor(){this.title="helloapp"}};i.\u0275fac=function(r){return new(r||i)},i.\u0275cmp=l({type:i,selectors:[["app-root"]],standalone:!0,features:[p],decls:41,vars:0,consts:[[1,"details"],[1,"name"],[1,"info"],[1,"edu"],[1,"educat"],[1,"school-detail"],[1,"school"],[1,"info-school"],[1,"skills"],[1,"ski"],[1,"skill-details"],[1,"deet-skill"],[1,"info-skill"]],template:function(r,k){r&1&&(e(0,"main")(1,"div",0)(2,"h2",1),n(3,"Bushrah Adebayo"),t(),e(4,"h3"),n(5,"LSDPC Estate, Yaba, Lagos State"),t(),e(6,"h3",2),n(7,"Telephone: 08123862952"),t()(),e(8,"div",3)(9,"h2",4),n(10,"EDUCATION"),t(),e(11,"div",5)(12,"div",6)(13,"h3"),n(14,"Ahmadu Bello University"),t(),e(15,"p"),n(16,"Bachelor of Science(B.Sc.), Computer Science"),t()(),e(17,"div",7)(18,"p"),n(19,"Kaduna, Nigeria"),t(),e(20,"h4"),n(21,"2021-2025"),t()()()(),e(22,"div",8)(23,"h2",9),n(24,"SKILLS & INTERESTS"),t(),e(25,"div",10)(26,"div",11)(27,"p"),n(28,"Skills"),t(),e(29,"p"),n(30,"Languages"),t(),e(31,"p"),n(32,"Interests"),t()(),e(33,"div",12)(34,"p"),n(35,"Microsoft Office Skill, Web Development Skill, Writing Skill"),t(),e(36,"p"),n(37,"English(fluent), Yoruba(fluent), Spanish(intermediate)"),t(),e(38,"p"),n(39,"Volunteering, Photography"),t()()()()(),a(40,"router-outlet"))},dependencies:[f],styles:[`main[_ngcontent-%COMP%]{
+    background: lightcoral;
+    color: black;
+}
+.details[_ngcontent-%COMP%]{
+    margin-top: 40px;
+    text-align: center;
+}
+.edu[_ngcontent-%COMP%]{
+    margin-top: 30px;
+}
+.educat[_ngcontent-%COMP%]{
+    border-bottom: 2px solid white;
+}
+.school-detail[_ngcontent-%COMP%]{
+    display: flex;
+    gap: 60px;
+    margin-top: 20px;
+}
+.ski[_ngcontent-%COMP%]{
+    border-bottom: 2px solid white;
+}
+.info-skill[_ngcontent-%COMP%]{
+    font-style: italic;
+    font-weight: 400;
+}
+.skills[_ngcontent-%COMP%]{
+    margin-top: 40px;
+}
+.skill-details[_ngcontent-%COMP%]{
+    display: flex;
+    gap: 50px;
+    margin-top: 20px;
+}`]});let o=i;return o})();var u=[];var S={providers:[g(u),d()]};var C={providers:[c()]},h=m(S,C);var E=()=>s(x,h),F=E;export{F as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-U3KU5BSO.mjs";import"./chunk-4VBI6W4Z.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
